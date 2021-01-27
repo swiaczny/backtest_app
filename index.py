6775b9860429a22142d0e3e948d49217e27bc73e
vdherrlichkeit <maritnswiaczny@gmail.com>
2021-01-27 11:12:24 +0100

    ..

--- index.py
+++ index.py
@@ -20,6 +20,7 @@ app = dash.Dash(__name__,suppress_callback_exceptions=True,external_stylesheets=
 # DUE TO MULTI-PAGE LAYOUT
 app.config.suppress_callback_exceptions = True
 server = app.server
+server.secret_key = os.environ.get('secret_key', 'secret')
 
 # LAYOUT --------------------------------------------------------------------------------------------------------------- 
 
